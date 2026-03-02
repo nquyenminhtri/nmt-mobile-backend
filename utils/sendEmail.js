210cd908e6ee9f8886054eeb051277ff0941d7a8
Nguyen Minh Tri <nmtri102work@gmail.com>
2026-03-02 15:55:44 +0700

    fix send mail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (to, otp) => {
   try {
-    const data = await resend.email.send({
+    const data = await resend.emails.send({
       from: "onboarding@resend.dev", // dùng tạm domain này
       to: to,
       subject: "Mã OTP xác nhận",
