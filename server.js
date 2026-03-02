ce7a3a3479b52158dfb5f3483b01b69ce336baf4
Nguyen Minh Tri <nmtri102work@gmail.com>
2026-03-03 00:34:00 +0700

    update manage bookings

--- server.js
+++ server.js
@@ -63,6 +63,18 @@ app.post("/api/send-otp", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+function verifyToken(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) return res.status(401).json({ message: "Không có token" });
+
+  jwt.verify(token, SECRET_KEY, (err, decoded) => {
+    if (err) return res.status(403).json({ message: "Token không hợp lệ" });
+
+    req.user = decoded;
+    next();
+  });
+}
 app.post("/api/verify-otp", async (req, res) => {
   try {
     const { phone, otp } = req.body;
